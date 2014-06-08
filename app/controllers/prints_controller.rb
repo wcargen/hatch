0aaeb4a8e2400f609acbef1b966f59eae9c94811
wcargen <wcargen@gmail.com>
2014-06-08 15:15:24 -0500

    Add routes for static print pages

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -1,4 +1,64 @@
 class PrintsController < ApplicationController
   def index
   end
+
+  def prints2013
+  end
+
+  def prints2012
+  end
+
+  def prints2011
+  end
+
+  def prints2010
+  end
+
+  def prints2009
+  end
+
+  def prints2008
+  end
+
+  def prints2007
+  end
+
+  def prints2006
+  end
+
+  def prints2005
+  end
+
+  def prints2004
+  end
+
+  def prints2003
+  end
+
+  def prints2002
+  end
+
+  def prints2001
+  end
+
+  def prints2000
+  end
+
+  def prints1999
+  end
+
+  def prints1998
+  end
+
+  def prints1997
+  end
+
+  def prints1996
+  end
+
+  def prints1995
+  end
+
+  def prints1994
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,25 @@ Hatch::Application.routes.draw do
 
   root :to => 'prints#index'
 
+  match '/prints2013', to: 'prints#prints2013'
+  match '/prints2012', to: 'prints#prints2012'
+  match '/prints2011', to: 'prints#prints2011'
+  match '/prints2010', to: 'prints#prints2010'
+  match '/prints2009', to: 'prints#prints2009'
+  match '/prints2008', to: 'prints#prints2008'
+  match '/prints2007', to: 'prints#prints2007'
+  match '/prints2006', to: 'prints#prints2006'
+  match '/prints2005', to: 'prints#prints2005'
+  match '/prints2004', to: 'prints#prints2004'
+  match '/prints2003', to: 'prints#prints2003'
+  match '/prints2002', to: 'prints#prints2002'
+  match '/prints2001', to: 'prints#prints2001'
+  match '/prints2000', to: 'prints#prints2000'
+  match '/prints1999', to: 'prints#prints1999'
+  match '/prints1998', to: 'prints#prints1998'
+  match '/prints1997', to: 'prints#prints1997'
+  match '/prints1996', to: 'prints#prints1996'
+  match '/prints1995', to: 'prints#prints1995'
+  match '/prints1994', to: 'prints#prints1994'
+
 end
